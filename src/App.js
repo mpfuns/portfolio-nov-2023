7aac10485f7e80ef9eaa3553606b747a33eec864
Monet <monetpaul23@gmail.com>
2023-11-26 21:41:52 -0500

    working on  languages

--- src/App.js
+++ src/App.js
@@ -45,6 +45,30 @@ function App() {
     <div className='modal__half modal__about'>
       <h3 className='modal__title modal__title--about'>Here's a bit  about me.</h3>
       <h4 className='modal__sub-title modal__sub-title--about'>Frontend Software Engineer</h4>
+      <p className='modal__para'>
+        I'm a 23 year-old <b className='special-color'>Frontend Software Engineer</b> with a strong passion for developing  websites with <b className='special-color'> great user experiences.</b>
+        <br></br>
+        I'm currently working  on extremely difficult engineering problems and learn from a team consisting of some of the most <b className='special-color'>talented</b> and <b className='special-color'>experienced</b> software engineers every day. 
+      </p>
+      <div className='modal__languages'>
+        <figure className='modal__language' >
+            <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/240px-HTML5_Badge.svg.png"/>
+            <span className='language__name'> HTML</span>
+        </figure>
+        <figure className='modal__language'>
+          <img src="https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png"/>
+        <span  className='language__name'> CSS</span>
+        </figure>
+        <figure className='modal__language'>
+          <img src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png"/>
+        <span  className='language__name'> Javascript</span>
+        </figure>
+        <figure className='modal__language'>
+        <img src="https://cdn.iconscout.com/icon/free/png-256/react-3-1175109.png"/> 
+        <span  className='language__name'> React</span>
+        </figure>
+
+      </div>
     </div>
     <div className='modal__half modal__contact'>Let's have a chat
     <h3 className='modal__title modal__title--contact'>Here's a bit  about me.</h3>
